7d5b8146fdfd69fddd00d46ab6090918408cff1d
HaeRhee <76435572+NSunR@users.noreply.github.com>
2023-04-08 12:07:12 +0900

    [level 0] Title: 두 수의 합, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120802. 두 수의 합/두 수의 합.js
+++ 프로그래머스/lv0/120802. 두 수의 합/두 수의 합.js
@@ -1,4 +1,4 @@
 function solution(num1, num2) {
     let numbers = [num1, num2]
-    return numbers.reduce((num1, num2) => num1 + num2, 0);
+    return numbers.reduce((num1, num2) => num1 + num2);
 }
